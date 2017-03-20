281987498c2bed997fa7108ca91fca28fbcfa4af
matxeyc <matxeyclima@gmail.com>
2017-03-20 15:02:05 -0300

    modificação do formulário De cadastro

--- Avalia.Model/UsuarioLogado.cs
+++ Avalia.Model/UsuarioLogado.cs
@@ -22,4 +22,27 @@ namespace Avalia.Model
         }
 
     }
+
+    //Exemplo de Sigleton
+   /* public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+
+    */
+
 }
